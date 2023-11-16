3a0d8e083104d3349b5aa1c7fb5291f3589dadc8
Keyvan <keyvanhojabr@gmail.com>
2023-11-16 18:35:32 +0330

    deployment

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
+import cors from 'cors';
+import { config } from 'dotenv';
 import express from "express";
-import {config} from 'dotenv'
-import router from "./route/index.js";
 import mongoose from "mongoose";
-import cors from 'cors'
-import { seedDoctors } from "./seedData.js";
-
+import path from 'path';
+import { fileURLToPath } from 'url';
+import router from "./route/index.js";
 config();
 const app = express()
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+console.log(__dirname);
 app.use(cors())
+app.use(express.static(__dirname + "/client"))
 app.use(express.json())
 app.use("/api", router)
+app.use("/*", (req, res) => {
+    res.sendFile(__dirname + "/client/index.html")
+})
 
 mongoose.connect(process.env.DB_ADDRESS).then(() => {
     console.log("db connected")
